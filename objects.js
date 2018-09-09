606ffcfe510d6ab7e0a1ccd8c5b90063f8c38cda
Nolan McGill <nomcgill@gmail.com>
2018-09-09 03:23:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,9 @@ var playlist = new Object({paramore:'decode'})
 
 function updatePlaylist(playlist,artist,song){
   playlist[artist] = song
+  return playlist
 }
+
+function removeFromPlaylist(playlist,artist,song)
+delete playlist.paramore
+ return playlist
